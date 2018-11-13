589dfb9bb83fe0733d6e4e00b94de9b2dc6c43af
irinache <irina.che333@gmail.com>
2018-11-13 10:42:41 +0200

    Add user psge and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get  '/contact',    to: 'static_pages#contact'
   get  '/news',    to: 'articles#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :articles
+  resources :users
 end
